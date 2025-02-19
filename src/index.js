003b70c0ee97d09916621b6322076a6b7a4b434f
Sai Nikhitha Madireddy <sainikhithamadireddy@gmail.com>
2025-02-19 15:28:44 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // Updated import for React 18
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+// Create the root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
